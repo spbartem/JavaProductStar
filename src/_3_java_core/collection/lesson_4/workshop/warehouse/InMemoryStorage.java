307d06ea552c927eb21429e62fc8a9bf9981afc0
spbartem <spbartemm@gmail.com>
2024-02-06 22:55:39 +0300

    Part of Lesson 4 (workshop, 46:15)

--- src/_3_java_core/collection/lesson_4/workshop/warehouse/InMemoryStorage.java
+++ src/_3_java_core/collection/lesson_4/workshop/warehouse/InMemoryStorage.java
@@ -2,9 +2,12 @@ package _3_java_core.collection.lesson_4.workshop.warehouse;
 
 import _3_java_core.collection.lesson_4.workshop.warehouse.exceptions.ItemNotFoundException;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class InMemoryStorage implements Storage {
 
@@ -44,5 +47,20 @@ public class InMemoryStorage implements Storage {
         for (Wheel item : items) {
             putItem(item);
         }
+    }
+
+    @Override
+    public Map<String, Wheel> getAllItems() {
+        return new HashMap<>(items);
+    }
+
+    @Override
+    public List<Wheel> getAllItemsSorted(Predicate<Wheel> predicate) {
+        return items.values().stream().filter(predicate)
+                .sorted(Comparator.comparing(Wheel::model)
+                        .thenComparing(Wheel::category)
+                        .thenComparing(Wheel::place)
+                        .thenComparing(Wheel::id))
+                .collect(Collectors.toList());
     }
 }
--- test/_3_java_core/collection/lesson_4/workshop/warehouse/InMemoryStorageTest.java
+++ test/_3_java_core/collection/lesson_4/workshop/warehouse/InMemoryStorageTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -75,5 +74,44 @@ class InMemoryStorageTest {
         Assertions.assertTrue(storage.containsItem("3"));
         Assertions.assertTrue(storage.containsItem("2"));
         Assertions.assertTrue(storage.containsItem("1"));
+    }
+
+    @Test
+    void getAll() {
+        Wheel hakka = new Wheel(UUID.randomUUID().toString(), "hakkapelita", "winter", "A", 10);
+        Wheel michelin = new Wheel(UUID.randomUUID().toString(), "michelin", "winter", "B", 10);
+        Wheel hakkaSummer = new Wheel(UUID.randomUUID().toString(), "hakkapelita", "summer", "A", 10);
+        Wheel nordman = new Wheel(UUID.randomUUID().toString(), "nordman", "winter", "A", 10);
+        Wheel noname = new Wheel(UUID.randomUUID().toString(), "noname", "winter", "A", 10);
+        storage.putAllItems(List.of(hakka, michelin, hakkaSummer, nordman, noname));
+
+        Map<String, Wheel> allItems = storage.getAllItems();
+
+        Assertions.assertEquals(5, allItems.size());
+
+        allItems.put("1", new Wheel("1", "hakkapelita", "winter", "A", 10));
+
+        Assertions.assertEquals(5, storage.getAllItems().size());
+    }
+
+    @Test
+    void getAllSortedByModel() {
+        Wheel hakka = new Wheel("1", "hakkapelita", "winter", "A", 10);
+        Wheel michelin = new Wheel("2", "michelin", "winter", "B", 10);
+        Wheel hakkaSummer = new Wheel("3", "hakkapelita", "summer", "A", 10);
+        Wheel nordman = new Wheel("4", "nordman", "winter", "A", 10);
+        Wheel noname = new Wheel("5", "noname", "winter", "A", 10);
+        List<Wheel> items = List.of(hakkaSummer, hakka, michelin, noname, nordman);
+        storage.putAllItems(items);
+
+        Set<String> exceptedModels = new HashSet(List.of("hakkapelita", "michelin", "noname"));
+
+        List<Wheel> allItemsSorted = storage.getAllItemsSorted(wheel -> exceptedModels.contains(wheel.model()));
+
+        List<Wheel> sortedOrder = List.of(hakkaSummer, hakka, michelin, noname);
+        Assertions.assertEquals(4, allItemsSorted.size());
+        Assertions.assertEquals(sortedOrder, allItemsSorted);
+
+
     }
 }
